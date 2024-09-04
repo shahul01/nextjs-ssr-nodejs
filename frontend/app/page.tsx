5bf05c8e49a80134eeefd69a6f1134308b674078
shahul01 <shahulhammeed01@gmail.com>
2024-09-05 02:43:56 +0530

    feat:fe: frontend gets todos from backend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,33 @@
+'use client';
+import { useEffect, useState } from "react";
+import { backendUrl } from "./utils/constants";
+
+
+async function getTodos() {
+    const todos = await fetch(`${backendUrl}/todos`);
+    const resTodos = await todos.json();
+    console.log(`resTodos: `, resTodos);
+    return resTodos;
+};
 
 export default function Home() {
+    const [ todos, setTodos ] = useState([]);
+
+    async function init() {
+        const newTodos = await getTodos();
+        setTodos(newTodos);
+    };
+
+    useEffect(() => {
+        init();
+    },[]);
+
+
   return (
     <main className="app">
+        {
+            JSON.stringify(todos,null, 2)
+        }
 
     </main>
   );
