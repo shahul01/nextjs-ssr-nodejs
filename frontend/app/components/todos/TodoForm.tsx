40bd96125b909323b5843942d35b8da5e97f5523
shahul01 <shahulhammeed01@gmail.com>
2024-09-05 07:55:16 +0530

    style:fe: added headless-ui, updated tailwind code

--- frontend/app/components/todos/TodoForm.tsx
+++ frontend/app/components/todos/TodoForm.tsx
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import { useFormState, useFormStatus } from "react-dom";
+import { Button, Input } from '@headlessui/react';
 import { createTodoAction } from "@/app/(pages)/actions";
 
 
@@ -7,12 +8,15 @@ import { createTodoAction } from "@/app/(pages)/actions";
 function SubmitButton() {
   const status = useFormStatus();
   return (
-    <button
-      className="text-white bg-blue-500 hover:bg-blue-700 font-bold px-2 rounded-sm"
+    <Button
+      className="inline-flex items-center gap-2 rounded-md py-1.5 px-3
+        bg-gray-700 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none
+        data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
       disabled={status.pending}
+      type={'submit'}
     >
       { status.pending ? 'Submitting...' : 'Submit' }
-    </button>
+    </Button>
   );
 };
 
@@ -29,11 +33,13 @@ export default function TodoForm() {
 
   return (
     <form ref={formRef} action={formAction}>
-      <input
-        type="text"
+      <Input
+        type='text'
         name='title'
         placeholder="title"
-        className="text-black border border-gray-400 mr-2 outline-none"
+        className="mt-3 rounded-lg border-none py-1.5 px-3
+        text-sm text-white bg-white/5
+        focus:outline-none focus:outline-2 focus:outline-offset-2 focus:outline-white/25"
       />
       <SubmitButton />
     </form>
